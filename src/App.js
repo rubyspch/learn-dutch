aa1b19ffc5a3b925155a29e2ff19cfe4388029bc
rubyspch <ruby_chohan@hotmail.co.uk>
2021-08-07 20:21:44 +0200

    Update App.js
    Create file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card />
+      <button id="next">Next</button>
     </div>
   );
 }
 
 export default App;
+
+{/*
+NEXT STEPS:
+- Next button reloads page / generates new word
+- clicking the dutch word reveals the translation
+*/}
