ba87981eb102ea3557d72bb5ecb0090e871c8664
rubyspch <ruby_chohan@hotmail.co.uk>
2021-08-09 15:25:23 +0200

    Update App.js
    Add page reload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
-import Card from './Card'
+import Card from './Card';
+// import Next from './Next';
 
 function App() {
+  function refresh(){
+    window.location.reload();
+  }
   return (
     <div className="App">
       <Card />
-      <button id="next">Next</button>
+      <button onClick={refresh} id="next">Next</button>
     </div>
   );
 }
 
 export default App;
-
-{/*
-NEXT STEPS:
-- Next button reloads page / generates new word
-- clicking the dutch word reveals the translation
-*/}
