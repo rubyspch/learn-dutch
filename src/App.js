314e742ee13c435f47b0ab3f3fb70c3a4fa5c4be
rubyspch <ruby_chohan@hotmail.co.uk>
2021-08-12 19:49:50 +0200

    Add styles and colour generator

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import Card from './Card';
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [colour, setColour] = useState("");
   function createNumber(){
     let number = Math.floor((Math.random() * 998));
     setNumber(number);
   }
+  function initColour(){
+    const str = "0123456789ABCDEF";
+    let hex = "#"
+    for (let i=0; i<6;i++){
+      let index = Math.floor(Math.random() * 16);
+      hex+= str[index];
+      console.log(hex);
+    }
+    // const styleObj = {
+    //   border:"3px solid rgb(233, 92, 92)",
+    //   boxShadow: "3px 3px 3px rgb(235, 92, 92)"
+    // }
+    console.log(hex);
+    setColour(hex);
+  }
+  console.log(colour);
+  initColour();
   return (
     <div className="App">
-      <Card number={number}/>
+      <Card colour={colour} number={number}/>
       <button onClick={createNumber} id="next">Next</button>
     </div>
   );
