110cd0115a78461bbe84c1b64726351fd5564a93
rubyspch <ruby_chohan@hotmail.co.uk>
2021-08-12 19:48:45 +0200

    Add styles and colour generator

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import {translations} from './Dutch-to-English'
 import './Card.css';
 
-function Card({number}){
+function Card({number, colour}){
     function reveal(){
         let wordElement = document.getElementsByClassName('word');
         let answerElement = document.getElementsByClassName('answer');
@@ -11,8 +11,16 @@ function Card({number}){
     }
     let word = translations[number].Dutch;
     let answer = translations[number].English;
+    console.log(colour);
+    // function colourAdd(){
+    //     console.log(colour);
+    //     const divContainer = document.getElementById('container');
+    //     divContainer.style.border = `3px solid rgb(${colour})`;
+    //     divContainer.style.boxShadow = `3px 3px 3px rgb(${colour})`;
+    // }
+    // window.onload = colourAdd();
     return (
-        <div>
+        <div id="container" style={{border:`3px solid rgb(${colour})`, boxShadow: `3px 3px 3px rgb(${colour})`}}>
             <div >
                 <h1 className="word" onClick={reveal}> 
                     {word}
