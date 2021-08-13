b2ede2972598356ba320e94c3ba20d6d158f882e
rubyspch <ruby_chohan@hotmail.co.uk>
2021-08-13 11:31:41 +0200

    Create Footer.js
    Create file

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div className="footer">
+            <p>Made with 🧡 and Javscript by <a href="https://github.com/rubyspch">rubyspch</a></p>
+        </div>
+    )
+}
+export default Footer;
